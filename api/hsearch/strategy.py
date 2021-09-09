def6ce0b2f8949d2129d46678a7db9853285de66
Alexander Astashov <alexander.astashov.vl@gmail.com>
2021-09-09 20:30:09 +0600

    Added check to older chat.

--- api/hsearch/strategy.py
+++ api/hsearch/strategy.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
 from social_django.strategy import DjangoStrategy
 
+from hsearch.models import Chat
+
 
 class SSOStrategy(DjangoStrategy):
     def create_user(self, *args, **kwargs):
@@ -8,8 +10,13 @@ class SSOStrategy(DjangoStrategy):
         title = kwargs.pop("fullname", "")
 
         user = self.storage.user.create_user(*args, **kwargs)  # type: User
-        user.chat.username = kwargs["username"]
-        user.chat.chat_id = chat_id
-        user.chat.title = title
-        user.chat.save(update_fields=["username", "chat_id", "title"])
+        chat = user.chat
+        if not chat.chat_id:
+            chat = Chat.objects.filter(chat_id=chat_id).first() or chat
+            chat.user = user
+
+        chat.username = kwargs["username"]
+        chat.chat_id = chat_id
+        chat.title = title
+        chat.save(update_fields=["user", "username", "chat_id", "title"])
         return user
